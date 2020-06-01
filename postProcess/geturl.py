5edbda4944e067d5696cfd7e060d5b6de05bab0a
Florian Landras <landrasflorian@gmail.com>
2020-06-01 22:05:02 +0200

    getUrl v0.1
    adding more choice possibilty in the type of house you want to buy ( in progress )
    v

--- postProcess/geturl.py
+++ postProcess/geturl.py
@@ -1,19 +1,37 @@
-#import post process
-
-# from urllib import request library don't fucking work / Use >>> requestS 
-from urllib.parse import urlencode
-from bs4 import BeautifulSoup
-import json
-import requests
-import re
-
- 
-baseUrl = 'https://www.hemnet.se/salda/villa/'
+baseUrl = 'https://www.hemnet.se/salda/'
 city = 'soderkopings-kommun'
+
+
+
+data = {'typeBostad': ['villa'],
+        'sizeMin': [""],
+        'sizeMax': [""],
+        'nbrOfRoomMin': [""],
+        'nbrOfRoomMax': [""],
+       
+        }
+
+df = pd.DataFrame(data)
+dfA = str(df["typeBostad"])
+dfB = str(df["sizeMin"] )
+dfC = str(df["sizeMax"])
+dfD = str(df["nbrOfRoomMin"])
+dfE = str(df["nbrOfRoomMax"])
+dfF = str(df["typeBostad"] )
+
+type(dfA)
+
+
+
 typeBostad = ''
 sizeMin =''
 sizeMax = ''
+nbrDeChambreMin = ""
+nbrDeChambreMax = ""
 
+
+def is_not_blank(s):
+    return bool(s and s.strip())
 
 url = baseUrl+city
 
@@ -22,9 +40,42 @@ def dataJson () :
     html = requests.get(url) 
     dataLayer = re.findall("dataLayer = (.+?);\n", html.text, re.S)
     varJson = json.loads(dataLayer[0])
-    monJson = varJson[1]["results"]["locations"][0]["id"]
-    print(monJson)
+    cityId = varJson[1]["results"]["locations"][0]["id"]
+    
 
-    return monJson
+    return cityId
 
 dataJson()
+
+def newUrl() :
+    
+    # si typeBostad & 
+    newUrl = ""
+    
+    if is_not_blank(typeBostad) == False and is_not_blank(sizeMin) == False and is_not_blank(sizeMax) == False and is_not_blank(sizeMin) and is_not_blank(sizeMin) :
+        
+        newUrl = baseUrl + "bostader?location_ids%5B%5D=" + str(dataJson()) + "&sold_age=all"
+    
+    if is_not_blank(typeBostad) == True and is_not_blank(sizeMin) == False and is_not_blank(sizeMax) == False and is_not_blank(sizeMin) and is_not_blank(sizeMin) :
+        
+        newUrl = baseUrl + "bostader?location_ids%5B%5D=" + str(dataJson()) + "&item_types%5B%5D="+ typeBostad + "&sold_age=all"
+    
+   #if is_not_blank(typeBostad) == False and is_not_blank(sizeMin) == True and is_not_blank(sizeMax) == False :
+        
+        newUrl = baseUrl + "bostader?location_ids%5B%5D=" + str(dataJson()) + "&item_types%5B%5D="+ typeBostad + "&sold_age=all"
+        
+    #if is_not_blank(typeBostad) == False and is_not_blank(sizeMin) == False and is_not_blank(sizeMax) == True :
+        
+        newUrl = baseUrl + "bostader?location_ids%5B%5D=" + str(dataJson()) + "&item_types%5B%5D="+ typeBostad + "&sold_age=all"
+        
+    #if is_not_blank(typeBostad) == True and is_not_blank(sizeMin) == True and is_not_blank(sizeMax) == True :
+        
+        newUrl = baseUrl + "bostader?location_ids%5B%5D=" + str(dataJson()) + "&item_types%5B%5D="+ typeBostad + "&sold_age=all"
+    return newUrl
+
+# nouvelle adresse format : https://www.hemnet.se/salda/bostader?location_ids%5B%5D=18002&item_types%5B%5D=villa&sold_age=all
+
+
+#newUrl = baseUrl + "bostader?location_ids%5B%5D=" + str(dataJson()) + "&sold_age=all"
+
+newUrl()
