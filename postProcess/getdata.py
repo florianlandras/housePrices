f7b246d884c6cc8db8842f81f5f0b74621f64ae5
Florian Landras <landrasflorian@gmail.com>
2020-06-05 19:45:29 +0200

    getdata
    creation . of the code to get the data and put it in dataframe
    Area and nbr of room done

---
+++ postProcess/getdata.py
@@ -0,0 +1,63 @@
+from urllib.parse import urlencode
+from bs4 import BeautifulSoup
+import json
+import requests
+import re
+import pandas as pd
+from urllib import request
+import numpy as np
+
+def getdef(url) :
+    html = requests.get(url)
+    soup = BeautifulSoup(html.text , 'html.parser')
+
+    areaArray = []
+    nbrRoomArray = []
+    priceArray = []
+    pricePerSquareMeterArray = []
+    adressArray = []
+
+
+# a / area + nbr of room
+    a = soup.find_all('div', class_ = "sold-property-listing__subheading sold-property-listing--left" )
+    for elem in a :
+        temp1 = re.findall(r"[-+]?\d*\.\d+|\d+", elem.get_text())
+        areaArray.append(temp1)
+    
+    df = pd.DataFrame(data=areaArray, columns=["Area","Rooms", "Rooms2"])
+    df.replace(to_replace=[None], value=np.nan, inplace=True)
+    df = df.fillna(0)
+    del df['Rooms2']
+
+    
+# b /adress
+    b = soup.find_all('span', class_ = "item-result-meta-attribute-is-bold item-link" )
+#b2 / adress city
+    b2 = soup.find_all('span', class_ = "item-link" )
+# c / final price
+    c = soup.find_all('span', class_ = "sold-property-listing__subheading sold-property-listing--left" )
+# d / % difference between market entrance price and final price
+    d = soup.find_all('div', class_ = "sold-property-listing__price-change" )
+# e / date de la vente
+    e = soup.find_all('div', class_ = "sold-property-listing__sold-date sold-property-listing--left" )
+#f / pricePerMeterSquare
+    f = soup.find_all('div', class_ = "sold-property-listing__price-per-m2 sold-property-listing--left" )
+# g/ surface non habitable
+    g = soup.find_all('div', class_ = "sold-property-listing__supplemental-area sold-property-listing--left" )
+# h/surface du terrain
+    h = soup.find_all('div', class_ = "sold-property-listing__land-area sold-property-listing--left" )
+# i / type de bien
+    i =  soup.find_all('span', class_ = "hide-element" )
+#j charge ???
+    j =  soup.find_all('div', class_ = "sold-property-listing__fee" )
+# k/ lien du bien immobilier pour plus d'infos
+    k = soup.find_all('a', class_ = "item-link-container" )
+    
+
+    return df
+
+if __name__ == "__main__":
+    baseUrl = 'https://www.hemnet.se/salda/'
+    city = 'soderkopings-kommun'
+    url = baseUrl + city
+    getdef(url)
